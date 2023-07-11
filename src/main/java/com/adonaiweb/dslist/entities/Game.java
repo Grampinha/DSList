3c15f08d629456bd245e6b7a1687ce86cd6cd042
Anderson Grampinha <andersongrampinha@gmail.com>
2023-07-11 12:09:33 -0300

    Games seed

--- src/main/java/com/adonaiweb/dslist/entities/Game.java
+++ src/main/java/com/adonaiweb/dslist/entities/Game.java
@@ -18,48 +18,34 @@ public class Game {
 	private Long id;
 	private String title;
 	
-	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
 	
+	@Column (columnDefinition = "TEXT")
 	private String shortDescription;
 	
+	@Column (columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
-			String shortDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, 
+			String imgUrl, String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Game other = (Game) obj;
-		return Objects.equals(id, other.id);
 	}
 
 	public Long getId() {
@@ -105,6 +91,14 @@ public class Game {
 	public String getImgUrl() {
 		return imgUrl;
 	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}	
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
@@ -125,6 +119,22 @@ public class Game {
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
 	} 
-	               
+	      
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}	
 
 }
