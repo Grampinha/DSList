01fac076cd8a1f16e76b78dc3e39c41860164e4a
Anderson Grampinha <andersongrampinha@gmail.com>
2023-07-11 11:14:47 -0300

    Project created

---
+++ src/test/java/com/adonaiweb/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adonaiweb.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
